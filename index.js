f5d29e4abcf4ec4202637f1277c40640c92c754b
Kyle Daigle <kyle.daigle@github.com>
2018-02-06 09:16:33 -0500

    Simple initial effort to get the diff/patch

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 module.exports = (robot) => {
   // Your code here
   robot.log('Yay, the app was loaded!')
+  robot.on('pull_request.synchronize', async context => {
+    context.log('Pull request is incoming...')
+    context.log(context.payload)
+
+    // Set the correct requestMedia
+    context.github.config.requestMedia = 'application/vnd.github.v3.patch'
+
+    const result = await context.github.pullRequests.get({owner: 'kdaigle', repo: 'testing', number: 1})
+    context.log(result.data)
+  })
 
   // For more information on building apps:
   // https://probot.github.io/docs/
